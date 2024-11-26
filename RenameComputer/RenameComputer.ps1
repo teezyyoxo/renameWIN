9e6c1294369289dadcbc0752a7c305c877093b12
teezyyoxo <teezyyoxo@users.noreply.github.com>
2024-11-26 13:24:05 -0500

    Update RenameComputer.ps1

--- RenameComputer/RenameComputer.ps1
+++ RenameComputer/RenameComputer.ps1
@@ -1,209 +1,180 @@
+# Print initial information about the script version and credits
+Write-Host "RenameComputer v1.4"
+Write-Host "Based on version 1.3 (latest) of Michael Niehaus' RenameComputer.ps1 script."
+Write-Host "EXCELLENT write-up on it on his blog, here: https://oofhours.com/2020/05/19/renaming-autopilot-deployed-hybrid-azure-ad-join-devices/"
 
-<#PSScriptInfo
+# This script will automatically rename an AD/AAD-bound Windows computer to the machine's serial number based on the following command:
+# "(Get-WmiObject -Class Win32_Bios | Select-Object -Last 1).SerialNumber" <-- this will print ONLY the serial number.
 
-.VERSION 1.3
-
-.GUID 3b42d8c8-cda5-4411-a623-90d812a8e29e
-
-.AUTHOR Michael Niehaus
-
-.COMPANYNAME
-
-.COPYRIGHT
-
-.TAGS
-
-.LICENSEURI
-
-.PROJECTURI
-
-.ICONURI
-
-.EXTERNALMODULEDEPENDENCIES 
-
-.REQUIREDSCRIPTS
-
-.EXTERNALSCRIPTDEPENDENCIES
-
-.RELEASENOTES
-Version 1.3: Only rename if the current name's prefix matches
-Version 1.2: Fixed PowerShell 5.1 bug (BiosSeralNumber)
-Version 1.1: Updated for AAD scenarios too.
-Version 1.0: Initial version.
-
-.PRIVATEDATA
-
-#>
-
-<# 
-
-.DESCRIPTION 
-Rename the computer using the logic contained in this script.  This will work for AD or AAD-joined devices, or even workgroup machines.
-Note that if the device is joined to AD, there must be connectivity to the domain and the computer must have the rights to rename itself.
-
-#> 
-
+#script starts
 [CmdletBinding()]
 Param(
-    [Parameter(Mandatory = $False)] [string] $Prefix = ""
+    [Parameter(Mandatory = $False)] [string] $Prefix = "",
+    [switch] $TestMode  # -t or -T for test mode
 )
 
+# Function to log and exit
+function Log-Exit {
+    param([string]$message, [int]$exitCode = 0)
+    Write-Host $message
+    Stop-Transcript
+    Exit $exitCode
+}
+
 # If we are running as a 32-bit process on an x64 system, re-launch as a 64-bit process
-if ("$env:PROCESSOR_ARCHITEW6432" -ne "ARM64")
-{
-    if (Test-Path "$($env:WINDIR)\SysNative\WindowsPowerShell\v1.0\powershell.exe")
-    {
-        & "$($env:WINDIR)\SysNative\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -ExecutionPolicy bypass -File "$PSCommandPath" -Prefix $Prefix
-        Exit $lastexitcode
+if ("$env:PROCESSOR_ARCHITEW6432" -ne "ARM64") {
+    try {
+        if (Test-Path "$($env:WINDIR)\SysNative\WindowsPowerShell\v1.0\powershell.exe") {
+            & "$($env:WINDIR)\SysNative\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -ExecutionPolicy bypass -File "$PSCommandPath" -Prefix $Prefix -TestMode:$TestMode
+            Exit $lastexitcode
+        }
+    } catch {
+        Log-Exit "Error: Failed to launch as 64-bit process." 1
     }
 }
 
 # Create a tag file just so Intune knows this was installed
-if (-not (Test-Path "$($env:ProgramData)\Microsoft\RenameComputer"))
-{
-    Mkdir "$($env:ProgramData)\Microsoft\RenameComputer"
+try {
+    if (-not (Test-Path "$($env:ProgramData)\Microsoft\RenameComputer")) {
+        Mkdir "$($env:ProgramData)\Microsoft\RenameComputer"
+    }
+    Set-Content -Path "$($env:ProgramData)\Microsoft\RenameComputer\RenameComputer.ps1.tag" -Value "Installed"
+    if ($TestMode) { Write-Verbose "Created tag file at $($env:ProgramData)\Microsoft\RenameComputer\RenameComputer.ps1.tag" }
+} catch {
+    Log-Exit "Error: Failed to create or write to the tag file." 1
 }
-Set-Content -Path "$($env:ProgramData)\Microsoft\RenameComputer\RenameComputer.ps1.tag" -Value "Installed"
 
 # Initialization
 $dest = "$($env:ProgramData)\Microsoft\RenameComputer"
-if (-not (Test-Path $dest))
-{
-    mkdir $dest
+try {
+    if (-not (Test-Path $dest)) {
+        mkdir $dest
+    }
+    Start-Transcript "$dest\RenameComputer.log" -Append
+    if ($TestMode) { Write-Verbose "Initialized log at $dest\RenameComputer.log" }
+} catch {
+    Log-Exit "Error: Failed to initialize the log directory." 1
 }
-Start-Transcript "$dest\RenameComputer.log" -Append
 
 # Bail out if the prefix doesn't match (if specified)
-Write-Host $Prefix
-$details = Get-ComputerInfo
-if (($Prefix -ne "") -and (-not $details.CsName.StartsWith($Prefix))) {
-    Write-Host "Device name doesn't match specified prefix, bailing out. Prefix=$Prefix ComputerName=$($details.CsName)"
-    Stop-Transcript
-    Exit 0
+if ($Prefix -ne "") {
+    $details = Get-ComputerInfo
+    if ($details.CsName -notlike "$Prefix*") {
+        Log-Exit "Device name doesn't match specified prefix. Prefix=$Prefix ComputerName=$($details.CsName)" 0
+    }
 }
 
-# See if we are AD or AAD joined
+# Detect if the computer is AD or AAD joined
 $isAD = $false
 $isAAD = $false
 $tenantID = $null
-if ($details.CsPartOfDomain) {
-    Write-Host "Device is joined to AD domain: $($details.CsDomain)"
-    $isAD = $true
-    $goodToGo = $false
-} else {
-    $goodToGo = $true
-    if (Test-Path "HKLM:/SYSTEM/CurrentControlSet/Control/CloudDomainJoin/JoinInfo") {
-        $subKey = Get-Item "HKLM:/SYSTEM/CurrentControlSet/Control/CloudDomainJoin/JoinInfo"
-        $guids = $subKey.GetSubKeyNames()
-        foreach($guid in $guids) {
-            $guidSubKey = $subKey.OpenSubKey($guid);
-            $tenantId = $guidSubKey.GetValue("TenantId");
+try {
+    $details = Get-ComputerInfo
+    if ($details.CsPartOfDomain) {
+        Write-Host "Device is joined to AD domain: $($details.CsDomain)"
+        if ($TestMode) { Write-Verbose "Device is part of Active Directory." }
+        $isAD = $true
+    } else {
+        if ($TestMode) { Write-Verbose "Device is not in AD, checking for AAD membership." }
+        if (Test-Path "HKLM:/SYSTEM/CurrentControlSet/Control/CloudDomainJoin/JoinInfo") {
+            $subKey = Get-Item "HKLM:/SYSTEM/CurrentControlSet/Control/CloudDomainJoin/JoinInfo"
+            $guids = $subKey.GetSubKeyNames()
+            foreach ($guid in $guids) {
+                $guidSubKey = $subKey.OpenSubKey($guid)
+                $tenantID = $guidSubKey.GetValue("TenantId")
+            }
+        }
+        if ($tenantID) {
+            Write-Host "Device is joined to AAD tenant: $tenantID"
+            if ($TestMode) { Write-Verbose "Device is part of Azure Active Directory." }
+            $isAAD = $true
+        } else {
+            Write-Host "Not part of a domain or AAD, in a workgroup."
         }
     }
-    if ($null -ne $tenantID) {
-        Write-Host "Device is joined to AAD tenant: $tenantID"
-        $isAAD = $true
-    } else {
-        Write-Host "Not part of a AAD or AD, in a workgroup."
-    }
+} catch {
+    Log-Exit "Error: Failed to detect AD or AAD domain membership." 1
 }
 
-# Make sure we have connectivity
-$goodToGo = $true
+# Check if connectivity to domain is good
 if ($isAD) {
-    $dcInfo = [ADSI]"LDAP://RootDSE"
-    if ($null -eq $dcInfo.dnsHostName)
-    {
-        Write-Host "No connectivity to the domain, unable to rename at this point."
-        $goodToGo = $false
+    try {
+        $dcInfo = [ADSI]"LDAP://RootDSE"
+        if ($null -eq $dcInfo.dnsHostName) {
+            Log-Exit "No connectivity to the domain, unable to rename at this point." 1
+        }
+    } catch {
+        Log-Exit "Error: Failed to check domain connectivity." 1
     }
 }
 
-# Good to go, we can rename the computer
-if ($goodToGo)
-{
-    # Remove the scheduled task (if it exists)
-    Disable-ScheduledTask -TaskName "RenameComputer" -ErrorAction Ignore
-    Unregister-ScheduledTask -TaskName "RenameComputer" -Confirm:$false -ErrorAction Ignore
-    Write-Host "Scheduled task unregistered."
+# If we're good to go, rename the computer
+if ($TestMode) {
+    Write-Host "Test Mode: The computer would be renamed but no changes will be made."
+    Write-Verbose "Would rename computer to: $newName"
+} else {
+    try {
+        # Remove existing scheduled task if it exists
+        Disable-ScheduledTask -TaskName "RenameComputer" -ErrorAction SilentlyContinue
+        Unregister-ScheduledTask -TaskName "RenameComputer" -Confirm:$false -ErrorAction SilentlyContinue
 
-    # Get the new computer name: use the asset tag (maximum of 13 characters), or the 
-    # serial number if no asset tag is available (replace this logic if you want)
-    $systemEnclosure = Get-CimInstance -ClassName Win32_SystemEnclosure
-    if (($null -eq $systemEnclosure.SMBIOSAssetTag) -or ($systemEnclosure.SMBIOSAssetTag -eq "")) {
-        # Stupid PowerShell 5.1 bug
-        if ($null -ne $details.BiosSerialNumber) {
+        $systemEnclosure = Get-CimInstance -ClassName Win32_SystemEnclosure
+        if ($null -eq $systemEnclosure.SMBIOSAssetTag) {
             $assetTag = $details.BiosSerialNumber
         } else {
-            $assetTag = $details.BiosSeralNumber
+            $assetTag = $systemEnclosure.SMBIOSAssetTag
         }
-    } else {
-        $assetTag = $systemEnclosure.SMBIOSAssetTag
-    }
-    if ($assetTag.Length -gt 13) {
-        $assetTag = $assetTag.Substring(0, 13)
-    }
-    if ($details.CsPCSystemTypeEx -eq 1) {
-        $newName = "D-$assetTag"
-    } else {
-        $newName = "L-$assetTag"
-    }
 
-    # Is the computer name already set?  If so, bail out
-    if ($newName -ieq $details.CsName) {
-        Write-Host "No need to rename computer, name is already set to $newName"
-        Stop-Transcript
-        Exit 0
-    }
+        if ($assetTag.Length -gt 13) {
+            $assetTag = $assetTag.Substring(0, 13)
+        }
 
-    # Set the computer name
-    Write-Host "Renaming computer to $($newName)"
-    Rename-Computer -NewName $newName -Force
+        $newName = if ($details.CsPCSystemTypeEx -eq 1) { "D-$assetTag" } else { "L-$assetTag" }
 
-    # Make sure we reboot if still in ESP/OOBE by reporting a 1641 return code (hard reboot)
-    if ($details.CsUserName -match "defaultUser")
-    {
-        Write-Host "Exiting during ESP/OOBE with return code 1641"
-        Stop-Transcript
-        Exit 1641
-    }
-    else {
-        Write-Host "Initiating a restart in 10 minutes"
-        & shutdown.exe /g /t 600 /f /c "Restarting the computer due to a computer name change.  Save your work."
-        Stop-Transcript
-        Exit 0
+        if ($newName -ieq $details.CsName) {
+            Log-Exit "No need to rename computer, name is already set to $newName" 0
+        }
+
+        Write-Host "Renaming computer to $newName"
+        Rename-Computer -NewName $newName -Force
+        if ($TestMode) { Write-Verbose "Successfully renamed computer to $newName" }
+
+        if ($details.CsUserName -match "defaultUser") {
+            Write-Host "Exiting during ESP/OOBE with return code 1641"
+            Log-Exit "Exiting during ESP/OOBE with return code 1641" 1641
+        } else {
+            Write-Host "Initiating a restart in 10 minutes."
+            & shutdown.exe /g /t 600 /f /c "Restarting the computer due to a computer name change.  Save your work."
+            Log-Exit "Restart initiated." 0
+        }
+    } catch {
+        Log-Exit "Error: Failed to rename the computer." 1
     }
 }
-else
-{
-    # Check to see if already scheduled
-    $existingTask = Get-ScheduledTask -TaskName "RenameComputer" -ErrorAction SilentlyContinue
-    if ($existingTask -ne $null)
-    {
-        Write-Host "Scheduled task already exists."
-        Stop-Transcript
-        Exit 0
+
+# Check and create the scheduled task if necessary
+if (-not $TestMode) {
+    try {
+        $existingTask = Get-ScheduledTask -TaskName "RenameComputer" -ErrorAction SilentlyContinue
+        if ($existingTask -eq $null) {
+            if (-not (Test-Path "$dest\RenameComputer.ps1")) {
+                Copy-Item $PSCommandPath "$dest\RenameComputer.ps1"
+            }
+            $action = New-ScheduledTaskAction -Execute "Powershell.exe" -Argument "-NoProfile -ExecutionPolicy bypass -WindowStyle Hidden -File $dest\RenameComputer.ps1"
+            $timespan = New-Timespan -Minutes 5
+            $triggers = @(
+                New-ScheduledTaskTrigger -Daily -At 9am
+                New-ScheduledTaskTrigger -AtLogOn -RandomDelay $timespan
+                New-ScheduledTaskTrigger -AtStartup -RandomDelay $timespan
+            )
+            Register-ScheduledTask -User SYSTEM -Action $action -Trigger $triggers -TaskName "RenameComputer" -Description "RenameComputer" -Force
+            Write-Host "Scheduled task created."
+        } else {
+            Write-Host "Scheduled task already exists."
+        }
+    } catch {
+        Log-Exit "Error: Failed to create or register the scheduled task." 1
     }
-
-    # Copy myself to a safe place if not already there
-    if (-not (Test-Path "$dest\RenameComputer.ps1"))
-    {
-        Copy-Item $PSCommandPath "$dest\RenameComputer.PS1"
-    }
-
-    # Create the scheduled task action
-    $action = New-ScheduledTaskAction -Execute "Powershell.exe" -Argument "-NoProfile -ExecutionPolicy bypass -WindowStyle Hidden -File $dest\RenameComputer.ps1"
-
-    # Create the scheduled task trigger
-    $timespan = New-Timespan -minutes 5
-    $triggers = @()
-    $triggers += New-ScheduledTaskTrigger -Daily -At 9am
-    $triggers += New-ScheduledTaskTrigger -AtLogOn -RandomDelay $timespan
-    $triggers += New-ScheduledTaskTrigger -AtStartup -RandomDelay $timespan
-    
-    # Register the scheduled task
-    Register-ScheduledTask -User SYSTEM -Action $action -Trigger $triggers -TaskName "RenameComputer" -Description "RenameComputer" -Force
-    Write-Host "Scheduled task created."
 }
 
 Stop-Transcript
